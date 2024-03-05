2e14b4b052bfbb80ea275a060f571bc063f09500
JohnMulishi <johnnorbert1151@gmail.com>
2024-03-05 21:49:19 +0000

    My Second commit for PLP Assignments

---
+++ user_input.py
@@ -0,0 +1,8 @@
+print("PLEASE ANSWER THE BELOW QUESTIONS\n")
+Name = input("Please enter your full name:\n")
+Age = input("Please enter your Age:\n")
+Location = input("Please enter your location:\n")
+print("Thank you for yor information:\n")
+print(f"Your Name is {Name}, your age is {Age} and you live at {Location}")
+print("THANK YOU")
+
